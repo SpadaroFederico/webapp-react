7940443666baea1ca16f5939be062681edf7aa3e
Federico Spadaro <federicospadaro2@gmail.com>
2025-03-13 15:29:55 +0100

    [FIXED] Home request from axios

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/movies')
+      .then(response => setMovies(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1>Lista dei Film</h1>
+      <ul className="list-group">
+        {movies.map(movie => (
+          <li key={movie.id} className="list-group-item">
+            <a href={`/movies/${movie.id}`}>{movie.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
