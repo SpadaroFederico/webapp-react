04c10dd30476b9000e5535b7dddd4e91c407a8c5
Federico Spadaro <federicospadaro2@gmail.com>
2025-03-13 15:27:23 +0100

    [FIXED] routing App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import { Routes } from './routes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import MovieDetail from './components/MovieDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  return <Routes />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
